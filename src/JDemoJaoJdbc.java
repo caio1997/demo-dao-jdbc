38a4bcd06750d06c2f3857867b77f6f495c29609
Caio Marinho <caio_mcoelho@hotmail.com>
2020-04-07 17:40:33 -0300

    Seller entity Class

--- src/JDemoJaoJdbc.java
+++ src/JDemoJaoJdbc.java
@@ -1,11 +1,17 @@
 
+import java.sql.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class JDemoJaoJdbc {
     public static void main(String[] args) {
-        
+        Date data = new Date(System.currentTimeMillis());
         Department dep = new Department(1, "Books");
+               
+        Seller seller = new Seller(1,"Caio","caio_mcoelho@hotmail.com",data,3000,dep);
+        
         System.out.println(dep);
+        System.out.println(seller);
     }
     
 }
