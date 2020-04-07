39ae3742b63c0be20968a492ea0a1e94b04eeae0
Caio Marinho <caio_mcoelho@hotmail.com>
2020-04-07 15:51:25 -0300

    Department entity Class

--- src/JDemoJaoJdbc.java
+++ src/JDemoJaoJdbc.java
@@ -1,6 +1,11 @@
+
+import model.entities.Department;
+
 public class JDemoJaoJdbc {
     public static void main(String[] args) {
         
+        Department dep = new Department(1, "Books");
+        System.out.println(dep);
     }
     
 }
