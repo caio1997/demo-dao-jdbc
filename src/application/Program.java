a6da96e5bf966e45799e3053077ac4cf63b8280f
Caio Marinho <caio_mcoelho@hotmail.com>
2020-04-07 18:00:53 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,22 @@
+package application;
 
 import java.sql.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class JDemoJaoJdbc {
+public class Program {
     public static void main(String[] args) {
         Date data = new Date(System.currentTimeMillis());
+        
         Department dep = new Department(1, "Books");
                
         Seller seller = new Seller(1,"Caio","caio_mcoelho@hotmail.com",data,3000,dep);
         
+        SellerDao selerDao = DaoFactory.createSellerDao();
+        
         System.out.println(dep);
         System.out.println(seller);
     }
-    
 }
