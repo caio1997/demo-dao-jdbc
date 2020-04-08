8a3556f04af0c26083a7b56712830be4d0ab834d
Caio Marinho <caio_mcoelho@hotmail.com>
2020-04-07 23:58:57 -0300

    findByDepartmente implememation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Scanner;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,11 +14,21 @@ public class Program {
         Scanner ler = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();  
         System.out.println("===== TEST 1: seller findById ========");
-        System.out.println("Digite o id que seja pesquisar");
+        System.out.print("Digite o id que seja pesquisar: ");;
         int resp = ler.nextInt();
         Seller seller = sellerDao.findById(resp);
+         System.out.println(seller);
+        
+        System.out.println("===== TEST 2: seller findDepartment ========");
+        System.out.print("Digite o id do departamento que seja pesquisar: ");
+        resp = ler.nextInt();
+        Department dep = new Department(resp, null);
+        List<Seller> list = sellerDao.findByDepartment(dep);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
         
        
-        System.out.println(seller);
+       
     }
 }
